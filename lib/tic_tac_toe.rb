f7f1c544c6f675b85119e1305a270890137ca8b0
allison cortez <allizetorc@gmail.com>
2019-11-02 18:21:02 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,10 +1,17 @@
 class TicTacToe
   
   def initialize
+    #sets the starting state of the board
+    #an array with 9 " " empty strings
+    #within an instance variable named @board
+    
     @board = Array.new(9, " ")
   end
   
   WIN_COMBINATIONS = [
+    #is a constant equal to a nested array 
+    #filled with index values for the various winning combinations possible
+    
     [0,1,2], #toprow
     [3,4,5], #middle
     [6,7,8],  #bottom
@@ -17,11 +24,25 @@ class TicTacToe
     ]
     
     def display_board
+      #prints the current board representation based on the @board instance variable
+      
       puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
       puts "-----------"
       puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
       puts "-----------"
       puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+    
+    def input_to_index(user_string)
+     #accepts the user's input (a string) as an argument
+     #converts the user's input (a string) into an integer
+     user_string.to_i
+     
+     
+     #converts the user's input from the user-friendly format (on a 1-9 scale) to the array-friendly format (where the first index starts at 0)
+      
+      
+      
     end
   
 end
